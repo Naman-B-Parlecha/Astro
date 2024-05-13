470f8cc8a0b3f12f6f568c16e9bc6cf66cb1798f
Naman-Parlecha <namanparlecha@gmail.com>
2024-05-13 17:09:19 +0530

    Added Charts and Header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import HeaderBox from "@/components/HeaderBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Naman" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Acess and manage your account transactions with ease."
+          />
+        </header>
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
--- lib/utils.ts
+++ lib/utils.ts
@@ -65,7 +65,7 @@ export const formatDateTime = (dateString: Date) => {
   };
 };
 
-export function formatAmount(amount: number): string {
+export function  formatAmount(amount: number): string {
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
