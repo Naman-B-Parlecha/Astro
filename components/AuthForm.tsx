347a6c064e1f648c96fda9c790b158ef3581164c
Naman-Parlecha <namanparlecha@gmail.com>
2024-05-17 23:38:52 +0530

    Added the initial ui of Auth Routes

---
+++ components/AuthForm.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const AuthForm = ({ type }: { type: string }) => {
+  const [user, setUser] = useState(null);
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-6 md:gap-8">
+        <Link href={"/"} className="cursor-pointer flex items-center gap-1">
+          <Image src="/icons/logo.svg" width={34} height={34} alt="logo" />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Horizon
+          </h1>
+        </Link>
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 text-gray-900">
+            {user ? "Link Acoount" : type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+          <p className="text-16 font-normal text-gray-600">
+            {user
+              ? "Link your account to get started"
+              : "Please enter your details"}
+          </p>
+        </div>
+      </header>
+      {user ? (
+        <div className="flex flex-col gap-4">{/* Plaidink */}</div>
+      ) : (
+        <>FORM</>
+      )}
+    </section>
+  );
+};
+
+export default AuthForm;
